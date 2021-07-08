22eac8583da7c9e43eca876ecae85937040c8fed
Vilde Voss Stabell <Vilde.voss.stabell@joubel.com>
2021-07-08 15:18:01 +0200

    HFP-3330 Setup project files

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import {H5PContext} from './context/H5PContext';
+
+// Load library
+H5P = H5P || {};
+H5P.TextGrouping = () => {
+  // TODO: Add stuff here
+};
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isProd = (nodeEnv === 'production');
+
+module.exports = {
+  mode: nodeEnv,
+  optimization: {
+    minimize: isProd,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true
+          }
+        }
+      })
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'h5p-text-grouping.css'
+    })
+  ],
+  entry: {
+    dist: './src/app.js'
+  },
+  output: {
+    filename: 'h5p-text-grouping.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  target: ['web', 'es6'],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(s[ac]ss|css)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: ''
+            }
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        test: /\.svg|\.jpg|\.png$/,
+        include: path.join(__dirname, 'src/images'),
+        type: 'asset/resource'
+      },
+      {
+        test: /\.woff$/,
+        include: path.join(__dirname, 'src/fonts'),
+        type: 'asset/resource'
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: (isProd) ? undefined : 'eval-cheap-module-source-map'
+};
