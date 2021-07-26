ede13de904a91d86cd5b9acd580b38fac1a927d1
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2021-07-26 10:54:22 +0200

    HFP-3333 Update jsdoc, add exact propTypes

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import exact from 'prop-types-exact';
 
 import './Category.scss';
 
@@ -7,7 +8,10 @@ import './Category.scss';
  * A Category renders a list of TextElements received
  * through parameters, a dropzone, a title and buttons
  * for collapsing and adding other TextElements.
- * @param params Parameters
+ * @param {object} props Props object containing
+ * @param {string} props.title The name of the category
+ * @param {JSX.Element[]} props.children Elements to be rendered inside the category
+ * @returns {JSX.Element} A single category element
  */
 export default function Category({ title, children }) {
   const heading = `${title} (${children.length})`;
@@ -22,7 +26,7 @@ export default function Category({ title, children }) {
   );
 }
 
-Category.propTypes = {
+Category.propTypes = exact({
   title: PropTypes.string.isRequired,
   children: PropTypes.arrayOf(PropTypes.element)
-};
+});
