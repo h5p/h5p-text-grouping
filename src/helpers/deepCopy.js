8343512de4ee33bacab1d83c94001791c9ab7ccd
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2021-07-29 08:46:44 +0200

    HFP-3339 Add jsdoc for deepCopy

--- src/helpers/deepCopy.js
+++ src/helpers/deepCopy.js
@@ -1,3 +1,10 @@
+/**
+ * Use with care.
+ * Does not work with the following types:
+ * Dates, functions, undefined, Infinity, RegExps, Maps, Sets, Blobs, FileLists, ImageDatas, sparse Arrays, Typed Arrays or other complex types
+ * @param {any} item item to copy
+ * @returns {any} deep copy of the item
+ */
 export default function deepCopy(item) {
   return JSON.parse(JSON.stringify(item));
 }
