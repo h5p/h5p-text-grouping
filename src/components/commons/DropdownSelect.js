b1186edabe419e2abbfcbfab4c81202c91a9ae8c
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2021-07-29 10:10:35 +0200

    HFP-3339 Write jsdoc and add proptypes to Dropdown

--- src/components/commons/DropdownSelect.js
+++ src/components/commons/DropdownSelect.js
@@ -1,13 +1,20 @@
 import React, { useEffect } from 'react';
+import PropTypes, { arrayOf } from 'prop-types';
+
 import './DropdownSelect.scss';
 
+/**
+ * Dropdown select menu with either multi or single selectable options
+ * @param {object} props Props object
+ * @returns {JSX.Element} Dropdown select menu
+ */
 export default function DropdownSelect({
   label,
   onChange,
   onClose,
   options,
   currentlySelectedIds,
-  multiSelectable
+  multiSelectable = false
 }) {
   useEffect(() => {
     window.addEventListener('click', onClose);
@@ -49,3 +56,13 @@ export default function DropdownSelect({
     </div>
   );
 }
+
+DropdownSelect.propTypes = {
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onClose: PropTypes.func.isRequired,
+  options: PropTypes.arrayOf(arrayOf(PropTypes.oneOfType([PropTypes.string, PropTypes.bool])))
+    .isRequired,
+  currentlySelectedIds: PropTypes.arrayOf(PropTypes.string).isRequired,
+  multiSelectable: PropTypes.bool
+};
