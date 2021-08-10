c8d2cf5a90de701fbb37ee3d731223bc0c1f8aaf
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2021-08-10 10:44:59 +0200

    HFP-3342 Use Iframe width and instance in hook

--- src/helpers/useNarrowScreen.js
+++ src/helpers/useNarrowScreen.js
@@ -1,21 +1,23 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { H5PContext } from '../context/H5PContext';
 
 /**
  * Utility hook for checking if device is mobile or not.
- * @returns {boolean}true if screen is less than or equal to 640 pixels, false otherwise.
+ * @returns {boolean} true if screen is less than or equal to 640 pixels, false otherwise.
  */
 const useNarrowScreen = () => {
+  const { instance } = useContext(H5PContext);
   const narrowScreenWidth = 640; // pixels
   const [screenIsNarrow, setScreensNarrow] = useState(false);
 
   function checkWindowSize() {
-    setScreensNarrow(window.innerWidth <= narrowScreenWidth);
+    setScreensNarrow(document.documentElement.clientWidth <= narrowScreenWidth);
   }
   useEffect(() => {
-    window.addEventListener('resize', checkWindowSize);
+    instance.document.addEventListener('resize', checkWindowSize);
     checkWindowSize();
     return () => {
-      window.removeEventListener('resize', checkWindowSize);
+      instance.document.removeEventListener('resize', checkWindowSize);
     };
   }, []);
 
