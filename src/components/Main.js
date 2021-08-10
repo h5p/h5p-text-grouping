b1a090131f9da6e8e14ded4089a69c388b724075
Vilde Voss Stabell <Vilde.voss.stabell@joubel.com>
2021-08-10 11:28:03 +0200

    Remove redundant code

--- src/components/Main.js
+++ src/components/Main.js
@@ -135,7 +135,6 @@ export default function Main({ context }) {
     dragState.textItemRef.current.style.zIndex = '';
     dragState.textItemRef.current.style.left = '';
     dragState.textItemRef.current.style.top = '';
-    dragState.textItemRef.current.children[0].classList.remove('text-item-selected');
 
     // Move text item to new category if it was dropped in a new category
     let insideCategoryIndex = -1;
@@ -301,7 +300,6 @@ export default function Main({ context }) {
     >
       <CategoryList
         categoryAssignment={categoryAssignment}
-        textGroups={textGroups}
         moveTextItems={moveTextItems}
         allTextItems={getRandomizedTextItems().slice()}
         removeAnimations={removeAnimations}
